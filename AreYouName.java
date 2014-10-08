f1ec861dafd0d95bd5491d65a29d5f2db82d72a9
kazui-siraisi <21311159ks@tama.ac.jp>
2014-10-08 15:43:07 +0900

    練習としてあなたの名前はというプログラムの追加

---
+++ AreYouName.java
@@ -0,0 +1,14 @@
+import java.*;
+public class AreYouName 
+{
+	public static void main (String[] args)
+	{
+	System.out.println("‚ ‚È‚½‚Ì–¼‘O‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+	BufferedReader reader = new BufferedReader (new InputStreamReader (System.in));
+	try
+		{
+		String line = reader.readLine();
+		System.out.println(line + "‚³‚ñA‚±‚ñ‚É‚¿‚í");
+		}
+	}
+}
